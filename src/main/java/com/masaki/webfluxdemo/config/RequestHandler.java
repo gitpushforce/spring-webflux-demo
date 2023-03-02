2c5ae886d21134750038bbe9f1d8a343e4b8cd45
masaki nakamura <gmk4n0n@gmail.com>
2023-03-03 00:12:41 +0900

    implementing router

---
+++ src/main/java/com/masaki/webfluxdemo/config/RequestHandler.java
@@ -0,0 +1,29 @@
+package com.masaki.webfluxdemo.config;
+
+import com.masaki.webfluxdemo.dto.Response;
+import com.masaki.webfluxdemo.service.ReactiveMathService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class RequestHandler {
+
+    @Autowired
+    private ReactiveMathService mathService;
+
+    public Mono<ServerResponse> squareHandler(ServerRequest serverRequest) {
+        int input = Integer.parseInt(serverRequest.pathVariable("input"));
+        Mono<Response> responseMono = this.mathService.findSquare(input);
+        return ServerResponse.ok().body(responseMono, Response.class);
+    }
+
+    public Mono<ServerResponse> tableHandler(ServerRequest serverRequest) {
+        int input = Integer.parseInt(serverRequest.pathVariable("input"));
+        Flux<Response> responseFlux = this.mathService.multiplicationTable(input);
+        return ServerResponse.ok().body(responseFlux, Response.class);
+    }
+}
